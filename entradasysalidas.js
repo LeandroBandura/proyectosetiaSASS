9f7cb2b134abc9ed2161b4dd794135eaa23050f3
Leandro Bandura <leandroezequielbandura@gmail.com>
2022-05-19 20:41:50 -0300

    agregando JS

---
+++ entradasysalidas.js
@@ -0,0 +1,9 @@
+let dni
+
+dni = prompt ("ingrese su DNI")
+
+if (dni == 37583950) {
+    alert ("Bienvenido al Sindicato de Empleados Textiles")
+} else if (dni != 37583950){
+    alert ("No estás afiliado. Por favor diríjete a la pestaña de 'AFILIACIONES'")
+} 
---
+++ seccionales.js
@@ -0,0 +1,8 @@
+let num = prompt ("dime tu edad")
+
+while (num < 18){
+
+    num = prompt("dime tu edad")
+}
+
+alert ("Bienvenido!")
